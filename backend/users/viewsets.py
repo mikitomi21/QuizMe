5934076e5e5482fd28dd058fc3b680067ebbcbad
mikitomi21 <kubastach2001@gmail.com>
2024-02-04 21:59:39 +0100

    Clean code

--- backend/users/viewsets.py
+++ backend/users/viewsets.py
@@ -9,11 +9,3 @@ from .serializers import UserDetailSerializer
 class CustomUserViewSets(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = UserDetailSerializer
-
-    @action(
-        methods=["POST"],
-        detail=True
-    )
-    def dupa(self, request):
-        print("Siemaneczko")
-        return True
